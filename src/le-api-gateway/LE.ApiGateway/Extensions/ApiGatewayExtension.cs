9f8ac9d0517c5b4ef7c62426aa82c3cce4263b7c
Nguyen-Tan-Xanh <nguyentanxanh0507@gmail.com>
2024-07-08 23:55:36 +0700

    Init source code

---
+++ src/le-api-gateway/LE.ApiGateway/Extensions/ApiGatewayExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace LE.ApiGateway.Extensions
+{
+    public static class ApiGatewayExtension
+    {
+        public static IApplicationBuilder UseHealthCheck(this IApplicationBuilder app, string path)
+        {
+            app.Use((ctx, next) =>
+            {
+                var requestPath = ctx.Request.Path.ToString();
+                if (requestPath.Equals(path))
+                {
+                    return ctx.Response.WriteAsync("ok");
+                }
+                return next.Invoke();
+            });
+
+            return app;
+        }
+    }
+}
